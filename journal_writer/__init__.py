69b6745821f40b3d01937114e005aae84b4faf26
Borja González Seoane <garaje@glezseoane.es>
2022-02-23 00:25:16 +0100

    Create isolated module

---
+++ journal_writer/__init__.py
@@ -0,0 +1,15 @@
+###########################################################
+#                      Journal Writer
+#
+#           A Borja G. Seoane's ad-hoc utility to
+#                   write his Cartapacio
+#
+# Copyright 2022 Borja González Seoane. All rights reserved
+###########################################################
+
+__maintainer__ = "Borja González Seoane"
+__email__ = "garaje@glezseoane.es"
+__version__ = "0.0.1"
+
+__copyright__ = "Copyright 2022 Borja González Seoane"
+__license__ = "All rights reserved"
